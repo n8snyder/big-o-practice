7044e32dcfb7440709e6d6bfe31c0f6d58f906f9
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-14 13:00:23 -0800

    Finish part 3

--- complexity.js
+++ complexity.js
@@ -30,71 +30,71 @@ and experiment with different inputs!
 */
 
 function logUpTo(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log(i);
-  }
+    for (let i = 1; i <= n; i++) {
+        console.log(i);
+    }
 }
 
 //Time Complexity: O(n)
 
 function logAtLeast10(n) {
-  for (let i = 1; i <= Math.max(n, 10); i++) {
-    console.log(i);
-  }
+    for (let i = 1; i <= Math.max(n, 10); i++) {
+        console.log(i);
+    }
 }
 //Time Complexity: O(n)
 
 function logAtMost10(n) {
-  for (let i = 1; i <= Math.min(n, 10); i++) {
-    console.log(i);
-  }
+    for (let i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
 }
 //Time Complexity: O(1)
 
 function onlyNumsAtEvenIndexes(nums) {
-  let numsAtEvens = [];
+    let numsAtEvens = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (i % 2 === 0) {
-      numsAtEvens.push(nums[i]);
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            numsAtEvens.push(nums[i]);
+        }
     }
-  }
 
-  return numsAtEvens;
+    return numsAtEvens;
 }
 //Time Complexity: O(nums)
 
 function runningSubtotals(nums) {
-  let subtotals = [];
+    let subtotals = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    let subtotal = 0;
-    for (let j = 0; j <= i; j++) {
-      subtotal += nums[j];
+    for (let i = 0; i < nums.length; i++) {
+        let subtotal = 0;
+        for (let j = 0; j <= i; j++) {
+            subtotal += nums[j];
+        }
+        subtotals.push(subtotal);
     }
-    subtotals.push(subtotal);
-  }
 
-  return subtotals;
+    return subtotals;
 }
 //Time Complexity: O(nums^2)
 
 const VOWELS = "aeiouAEIOU";
 
 function vowelsCounts(word) {
-  let vowelsToCounts = {};
+    let vowelsToCounts = {};
 
-  for (let char of word) {
-    if (VOWELS.includes(char)) {
-      if (char in vowelsToCounts) {
-        vowelsToCounts[char] += 1;
-      } else {
-        vowelsToCounts[char] = 1;
-      }
+    for (let char of word) {
+        if (VOWELS.includes(char)) {
+            if (char in vowelsToCounts) {
+                vowelsToCounts[char] += 1;
+            } else {
+                vowelsToCounts[char] = 1;
+            }
+        }
     }
-  }
 
-  return vowelsToCounts;
+    return vowelsToCounts;
 }
 
 //Time Complexity: O(word)
@@ -104,27 +104,45 @@ function vowelsCounts(word) {
 Step Three: Short answer»
 Answer the following questions
 
-True or false: n2 + n is O(n2).
+True or false: n^2 + n is O(n^2).
+True
 
-True or false: n2 * n is O(n^3).
+True or false: n^2 * n is O(n^3).
+True
 
-True or false: n2 + n is O(n).
+True or false: n^2 + n is O(n).
+False
 
 What’s the time complexity of the .indexOf array method?
+It must go through whole array to look for the element (in worst casee)
+O(n)
 
 What’s the time complexity of the .includes array method?
+It must go through whole array to look for the element (in worst casee)
+O(n)
 
 What’s the time complexity of the .every array method?
+Applies the callback to every element in the array.
+O(n*m) where n in number of elements in array and m is the runtime of the callback.
 
 What’s the time complexity of the .sort array method?
+O(n log n)
 
 What’s the time complexity of the .unshift array method?
+Each element in the array must be moved, so it's O(n).
 
 What’s the time complexity of the .push array method?
+O(1) because only need to change the last empty poosition.
 
 What’s the time complexity of the .pop array method?
+O(1) because only effects the last element.
 
 What’s the time complexity of the Object.keys() function?
+Object.keys({a:1, b:2}) === [a, b]
+The function iterates over the keys, which is O(n).
+Because keys might not always be in order, the keys need to be sorted by the order they were added.
+Sorting is worst case O(n log n), but keys will usually always be in the correct order,
+so in practise will be O(n).
 
 Bonus:
 

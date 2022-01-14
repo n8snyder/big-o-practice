e596b0ebc5d48a3019d5af81b2b5390af427c9a8
Andrew Koo <andrewkykoo@gmail.com>
2022-01-14 12:29:34 -0800

    part 2 completed

--- complexity.js
+++ complexity.js
@@ -22,3 +22,111 @@
 // O(n x log(n)**10)
 
 // Part 2
+
+/*Step Two: Calculating Time Complexity»
+Determine the time complexities for each of the following functions. 
+If you’re not sure what these functions do, copy and paste them into the console
+and experiment with different inputs!
+*/
+
+function logUpTo(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+//Time Complexity: O(n)
+
+function logAtLeast10(n) {
+  for (let i = 1; i <= Math.max(n, 10); i++) {
+    console.log(i);
+  }
+}
+//Time Complexity: O(n)
+
+function logAtMost10(n) {
+  for (let i = 1; i <= Math.min(n, 10); i++) {
+    console.log(i);
+  }
+}
+//Time Complexity: O(1)
+
+function onlyNumsAtEvenIndexes(nums) {
+  let numsAtEvens = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      numsAtEvens.push(nums[i]);
+    }
+  }
+
+  return numsAtEvens;
+}
+//Time Complexity: O(nums)
+
+function runningSubtotals(nums) {
+  let subtotals = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let subtotal = 0;
+    for (let j = 0; j <= i; j++) {
+      subtotal += nums[j];
+    }
+    subtotals.push(subtotal);
+  }
+
+  return subtotals;
+}
+//Time Complexity: O(nums^2)
+
+const VOWELS = "aeiouAEIOU";
+
+function vowelsCounts(word) {
+  let vowelsToCounts = {};
+
+  for (let char of word) {
+    if (VOWELS.includes(char)) {
+      if (char in vowelsToCounts) {
+        vowelsToCounts[char] += 1;
+      } else {
+        vowelsToCounts[char] = 1;
+      }
+    }
+  }
+
+  return vowelsToCounts;
+}
+
+//Time Complexity: O(word)
+
+// Part 3
+/*
+Step Three: Short answer»
+Answer the following questions
+
+True or false: n2 + n is O(n2).
+
+True or false: n2 * n is O(n^3).
+
+True or false: n2 + n is O(n).
+
+What’s the time complexity of the .indexOf array method?
+
+What’s the time complexity of the .includes array method?
+
+What’s the time complexity of the .every array method?
+
+What’s the time complexity of the .sort array method?
+
+What’s the time complexity of the .unshift array method?
+
+What’s the time complexity of the .push array method?
+
+What’s the time complexity of the .pop array method?
+
+What’s the time complexity of the Object.keys() function?
+
+Bonus:
+
+What’s the space complexity of the Object.keys() function?
+*/
